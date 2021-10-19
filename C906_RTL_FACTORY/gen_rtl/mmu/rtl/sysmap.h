aba455425d6ff74b8050b03098f7dee90464a4bf
Ziyi Hao <tianzhidao44@gmail.com>
2021-10-19 14:07:13 +0800

    fix file header description

--- C906_RTL_FACTORY/gen_rtl/mmu/rtl/sysmap.h
+++ C906_RTL_FACTORY/gen_rtl/mmu/rtl/sysmap.h
@@ -1,28 +1,17 @@
-// *****************************************************************************
-// *                                                                           *
-// * C-Sky Microsystems Confidential                                           *
-// * -------------------------------                                           *
-// * This file and all its contents are prsrcties of C-Sky Microsystems. The  *
-// * information contained herein is confidential and proprietary and is not   *
-// * to be disclosed outside of C-Sky Microsystems except under a              *
-// * Non-Disclosure Agreement (NDA).                                           *
-// *                                                                           *
-// *****************************************************************************
-// FILE NAME       : ct_mmu_sysmap.vp
-// AUTHOR          : Ziyi Hao
-// ORIGINAL TIME   :
-// FUNCTION        : I-uTLB:
-//                 : 1. 16-entry utlb 
-//                 : 2. translate Va to PA
-//                 : 3. visit jTLB when uTLB miss
-//                 : 4. refill uTLB with PLRU algorithm
-// RESET           : 
-// DFT             :
-// DFP             :
-// VERIFICATION    :
-// RELEASE HISTORY :
-// $Id: sysmap.h,v 1.4 2020/10/19 01:03:12 sunc Exp $
-// *****************************************************************************
+/*Copyright 2020-2021 T-Head Semiconductor Co., Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 
 // ADDR is 28-bit, 4K address
 // Flag includes: Strong Order, Cacheable, Bufferable, Shareable, Security
